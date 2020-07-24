a11a578fee6d7675ba4f01b9b86e35b9f1178c1e
apzl <apsalshbk550@gmail.com>
2020-07-24 18:25:02 +0530

    backend

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,6 @@ from accounts import views as user_views
 urlpatterns = [
     path('signup/', user_views.registerPage, name="signup"),
 	path('profile/', user_views.profilePage, name="profile"),
-	path('match/', user_views.matchPage, name="match"),
 	path('login/', auth_views.LoginView.as_view(template_name='login.html') , name="login"),
 	path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout") 
 ]
